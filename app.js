8c4c1052775e53e8e7f657b6c20c2a4b3cfd4830
akashgajwar <75195665+akashgajwar@users.noreply.github.com>
2020-12-30 01:05:18 +0530

    final commit

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ var btnOne = document.querySelector("#inc");
 var btnTwo = document.querySelector("#dec");
 var outpurCounter = document.querySelector("#output");
 
-var count = 1;
+var count = 0;
 function clickHandlerOne(){
     count++ ;
+    outpurCounter.innerText = count;
 }
 
 function clickHandlerTwo(){
     count--;
+    outpurCounter.innerText = count;
 
 }
-
-outpurCounter.innerText = count;
-
-
 
 btnOne.addEventListener("click", clickHandlerOne);
 btnTwo.addEventListener("click", clickHandlerTwo);
